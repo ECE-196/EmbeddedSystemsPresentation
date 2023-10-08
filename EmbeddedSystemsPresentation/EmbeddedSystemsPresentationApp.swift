9b9f0b13b267483aa76566f5a4037752cb726517
Adin Ackerman <adinackerman@gmail.com>
2023-10-08 10:31:39 -0700

    Initial Commit

---
+++ EmbeddedSystemsPresentation/EmbeddedSystemsPresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmbeddedSystemsPresentationApp.swift
+//  EmbeddedSystemsPresentation
+//
+//  Created by Adin Ackerman on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct EmbeddedSystemsPresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
